494cd3ca96b9e3660f21ae9e919ab49d6ec37dfd
Pavel1095 <9153220501@mail.ru>
2022-08-03 17:43:16 +0300

    Второе задание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.printf("ФИО сотрудника - %s", fullName);
+        System.out.printf("ФИО сотрудника - %s\n", fullName);
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase(Locale.ROOT));
+
     }
 }
